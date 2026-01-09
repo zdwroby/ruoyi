c29eef8dc9a977da6e090547d4e28f7fa07686cc
kasang <kasangv2.0@gmail.com>
2026-01-09 14:54:13 +0800

    feat: 更新

--- ruoyi-ui/vue.config.js
+++ ruoyi-ui/vue.config.js
@@ -9,7 +9,7 @@ const CompressionPlugin = require('compression-webpack-plugin')
 
 const name = process.env.VUE_APP_TITLE || '若依管理系统' // 网页标题
 
-const baseUrl = 'http://localhost:9080' // 后端接口
+const baseUrl = 'http://localhost:17080' // 后端接口
 
 const port = process.env.port || process.env.npm_config_port || 80 // 端口
 
